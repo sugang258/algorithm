70110b754108b707b27e89dafc006f01bc975e25
Hongsugyeong <olivia258@naver.com>
2024-02-18 11:22:04 +0900

    4.단어 뒤집기

---
+++ src/main/java/단어_뒤집기_4.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class 단어_뒤집기_4 {
+
+    public StringBuilder Solution(String str) {
+
+        StringBuilder st = new StringBuilder();
+
+        for (int i = str.length()-1; i >= 0; i--) {
+            st.append(str.charAt(i));
+        }
+        return st;
+    }
+
+    public static void main(String[] args) {
+        단어_뒤집기_4 T = new 단어_뒤집기_4();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            String str = sc.next();
+            System.out.println(T.Solution(str));
+        }
+    }
+}
