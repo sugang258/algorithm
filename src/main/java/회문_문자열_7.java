95d786b97c3bb2d6aca287318292137ec9405e85
Hongsugyeong <olivia258@naver.com>
2024-02-20 20:41:11 +0900

    7.회문 문자열_2

--- src/main/java/회문_문자열_7.java
+++ src/main/java/회문_문자열_7.java
@@ -4,40 +4,13 @@ import java.util.Stack;
 public class 회문_문자열_7 {
 
     public String Solution(String str) {
-        Stack<Character> stack = new Stack<>();
         str = str.toLowerCase();
         int size = str.length();
 
-        for (int i = 0; i < size/2; i++) {
-            stack.add(str.charAt(i));
+        for(int i=0;i<size/2;i++) {
+            if(str.charAt(i) != str.charAt(size-i-1)) return "NO";
         }
-
-        //짝수일 때
-        if(size % 2 == 0) {
-            for(int i=0;i<size/2;i++) {
-                if(stack.peek() == str.charAt(i+(size/2))) {
-                    stack.pop();
-                }else{
-                    return "NO";
-                }
-            }
-        }
-
-        //홀수일 때
-        if(size % 2 != 0) {
-            for(int i = 0; i<size/2;i++) {
-                if(stack.peek() == str.charAt(i + (size/2)+1)) {
-                    stack.pop();
-                }else {
-                    return "NO";
-                }
-            }
-        }
-
-        if(stack.isEmpty()) {
-            return "YES";
-        }
-        return "NO";
+        return "YES";
     }
 
     public static void main(String[] args) {
