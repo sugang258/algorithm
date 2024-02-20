547d191ff6f60a90c76a95b377629cfd7bc0c4b8
Hongsugyeong <olivia258@naver.com>
2024-02-20 20:51:56 +0900

    8.유효한 팰린드롬_2

--- src/main/java/유효한_팰린드롬_8.java
+++ src/main/java/유효한_팰린드롬_8.java
@@ -1,51 +1,15 @@
 import java.util.Scanner;
-import java.util.Stack;
 
 public class 유효한_팰린드롬_8 {
 
     public String Solution(String str) {
-        Stack<Character> stack = new Stack<>();
-        String s = "";
+        str = str.toUpperCase().replaceAll("[^A-Z]","");
 
-        for (int i = 0; i < str.length(); i++) {
-            if(Character.isAlphabetic(str.charAt(i))) {
-                s += str.charAt(i);
-            }
-        }
-        s = s.toLowerCase();
-        int size = s.length();
+        String tmp = new StringBuilder(str).reverse().toString();
 
-        for (int i = 0; i < size/2; i++) {
-            stack.add(s.charAt(i));
-        }
+        if(str.equals(tmp)) return "YES";
 
-        //짝수일 때
-        if(size % 2 == 0) {
-            for(int i=0;i<size/2;i++) {
-                if(stack.peek() == s.charAt(i+(size/2))) {
-                    stack.pop();
-                }else{
-                    return "NO";
-                }
-            }
-        }
-
-        //홀수일 때
-        if(size % 2 != 0) {
-            for(int i = 0; i<size/2;i++) {
-                if(stack.peek() == s.charAt(i + (size/2)+1)) {
-                    stack.pop();
-                }else {
-                    return "NO";
-                }
-            }
-        }
-
-        if(stack.isEmpty()) {
-            return "YES";
-        }
         return "NO";
-
     }
 
     public static void main(String[] args) {
