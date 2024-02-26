068b0040a4ac853108aaa54a48e958cdc252e081
Hongsugyeong <olivia258@naver.com>
2024-02-26 15:30:31 +0900

    1.큰 수 출력하기_2

--- src/main/java/Array/큰_수_출력하기_1.java
+++ src/main/java/Array/큰_수_출력하기_1.java
@@ -1,37 +1,34 @@
 package Array;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.Buffer;
+import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class 큰_수_출력하기_1 {
 
-    public String Solution(int[] ar) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(ar[0] + " ");
+    public ArrayList<Integer> Solution(int[] ar, int n) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(ar[0]);
 
-        for(int i=1;i<ar.length;i++) {
+        for(int i=1;i<n;i++) {
             if(ar[i] > ar[i-1]) {
-                sb.append(ar[i] + " ");
+                arrayList.add(ar[i]);
             }
         }
-        return String.valueOf(sb);
+        return arrayList;
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         큰_수_출력하기_1 T = new 큰_수_출력하기_1();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
         int[] ar = new int[n];
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
         for(int i=0;i<n;i++) {
-            ar[i] = Integer.parseInt(st.nextToken());
+            ar[i] = sc.nextInt();
         }
-        System.out.println(T.Solution(ar));
+
+        for(int x : T.Solution(ar,n)) {
+            System.out.print(x + " ");
+        }
     }
 }
