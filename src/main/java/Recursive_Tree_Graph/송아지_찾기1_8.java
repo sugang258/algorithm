acc0274b4316d9db9c107fa50ff395cc898dae12
Hongsugyeong <olivia258@naver.com>
2024-03-20 19:58:59 +0900

    8.송아지 찾기1

---
+++ src/main/java/Recursive_Tree_Graph/송아지_찾기1_8.java
@@ -0,0 +1,44 @@
+package Recursive_Tree_Graph;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class 송아지_찾기1_8 {
+    int[] dis = {1, -1, 5};
+    int[] ch;
+    Queue<Integer> Q = new LinkedList<>();
+
+    public int BFS(int s, int e) {
+        ch = new int[10001];
+        ch[s] = 1;
+        Q.offer(s);
+        int L = 0;
+        while(!Q.isEmpty()) {
+            int len = Q.size();
+            for (int i = 0; i < len; i++) {
+                int x = Q.poll();
+                for (int j = 0; j < 3; j++) {
+                    int nx = x + dis[j];
+                    if (nx == e) return L+1;
+                    if (nx>=1 && nx <= 10000 && ch[nx] == 0) {
+                        ch[nx] = 1;
+                        Q.offer(nx);
+                    }
+                }
+            }
+            L++;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        송아지_찾기1_8 T = new 송아지_찾기1_8();
+        Scanner sc = new Scanner(System.in);
+
+        int s = sc.nextInt();
+        int e = sc.nextInt();
+
+        System.out.println(T.BFS(s,e));
+    }
+}
